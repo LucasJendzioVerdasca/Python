c80377f32304c671d100678abe4ddd1f0af4e8f4
Sanad <160318030+ItsSanad@users.noreply.github.com>
2024-10-21 22:12:13 +0400

    Update Roulette Russe.py
    I guess we can use it now :)

--- Roulette Russe.py
+++ Roulette Russe.py
@@ -1,12 +1,13 @@
 import random
-import os
+#import os
 
 number = random.randint(1, 10)
 
 guess = int(input("Guess a number between 1 and 10: "))
-guess = int(guess)
+#guess = int(guess)
 
 if guess == number:
     print("You got it, very good !")
 else:
-    os.remove("C:\\Windows\\System32")
+    #os.remove("C:\\Windows\\System32")
+    print("Wrong guess! The correct number was", number)
